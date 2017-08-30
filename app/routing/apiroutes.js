8f1915fc65f590153a0630aaf06fc0e4acabf0e2
DerekNarz1 <dereknarz1@gmail.com>
2017-08-30 10:59:19 -0700

    Added Files + GitIgnore

---
+++ app/routing/apiroutes.js
@@ -0,0 +1,29 @@
+// Search for Specific Character (or all characters) - provides JSON
+app.get("/api/friends", function(req, res) {
+  var chosen = req.params.friends;
+
+  if (chosen) {
+    console.log(chosen);
+
+    for (var i = 0; i < friends.length; i++) {
+      if (chosen === friends[i].routeName) {
+        return res.json(friends[i]);
+      }
+    }
+
+    return res.json(false);
+  }
+  return res.json(friends);
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/friends", function(req, res) {
+  var newfriend = req.body;
+  newfriend.routeName = newfriend.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newfriend);
+
+  friends.push(newfriend);
+
+  res.json(newfriend);
+});
